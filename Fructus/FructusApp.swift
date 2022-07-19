ff66a0a7d1fcc3a27afc0be671bc7e5b0febac35
Egor <egormolchanov1401@gmail.com>
2022-07-19 12:34:55 +0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Egor Molchanov on 7/19/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
